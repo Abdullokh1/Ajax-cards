1ae0a4eba9300b74db2385fe2331d1bf446a2443
Abdullokh1 <giyasovabdullo0@gmail.com>
2022-04-19 09:58:51 +0500

    converted to fetch

--- js/main.js
+++ js/main.js
@@ -1,34 +1,23 @@
-window.addEventListener('load', loadHTTP);
-
-function loadHTTP(){
-  let outrequest = new XMLHttpRequest();
-  let url = "https://reqres.in/api/users";
-  outrequest.open("GET", url);
-
-  outrequest.onload = function(){
-    let outdata = JSON.parse(outrequest.responseText);
-    renderHtml(outdata);
-  }
-
-  outrequest.send();
-}
-
+window.addEventListener('load', getApi);
 
 const list = document.querySelector('.card');
 
-function renderHtml(data){
-  
-  data.data.forEach(element => {
+
+function getApi(){
+  fetch("https://reqres.in/api/users")
+  .then((res) => res.json())
+  .then ((data) => {
+    data.data.forEach((user) =>{
     let li = document.createElement('li');
     li.className = 'col-lg-3 col-xs-12 col-sm-4 col-md-4 card__item';
     li.innerHTML = `
     <div class="d-flex align-items-center p-3">
       <div class="me-2">
-        <img class="card__img" src="${element.avatar}" alt="hero-img" width='50' height='50'>
+        <img class="card__img" src="${user.avatar}" alt="hero-img" width='50' height='50'>
       </div>
       <div>
-        <h3 class="m-0  card__title">${element.first_name}</h3>
-        <h4 class="card__surname m-0">${element.last_name}</h4>
+        <h3 class="m-0  card__title">${user.first_name}</h3>
+        <h4 class="card__surname m-0">${user.last_name}</h4>
       </div>
      <button class="card__btn">Remove User</button>
     </div>
@@ -45,7 +34,7 @@ function renderHtml(data){
       <li class="card__inner__item mb-3">
         <div class="d-flex justify-content-between mb-3 ps-3 pe-3">
           <p class="m-0 card__company">Email</p>
-          <p class="m-0 card__email">${element.email}</p>
+          <p class="m-0 card__email">${user.email}</p>
         </div>
       </li>
 
@@ -65,20 +54,18 @@ function renderHtml(data){
     </ul>
     `;
     list.appendChild(li);
-  
+    })
+
     const buttons = document.querySelectorAll('.card__btn');
-    buttons.forEach(el =>{
-      el.addEventListener('click', ()=>{
-        el.parentElement.parentElement.classList.add('removed');
-        list.addEventListener('transitionend', ()=>{
-          el.parentElement.parentElement.remove();
-        })
-      })
-    })   
-
-  });
+        buttons.forEach(el =>{
+          el.addEventListener('click', ()=>{
+            el.parentElement.parentElement.classList.add('removed');
+            list.addEventListener('transitionend', ()=>{
+              el.parentElement.parentElement.remove();
+            })
+          })
+        })   
+  })
 }
-
-
 
 
